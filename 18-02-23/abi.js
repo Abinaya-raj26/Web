91d68b7b5bb8fc92f8ff9cd9134410cb0dc28d76
Abinaya-raj26 <abinayaraj216@gmail.com>
2023-02-18 11:28:19 -0800

    18-02-23

---
+++ 18-02-23/abi.js
@@ -0,0 +1,33 @@
+var school="student name";
+
+function stuclass(){
+    document.getElementById("if").innerHTML=school;
+}
+
+function stucolor(){
+    document.getElementById("if").style.color="pink";
+}
+function teacherblock(){
+    document.getElementById("if").style.display="block";
+}
+function marknone(){
+    document.getElementById("if").style.display="none";
+}
+
+var x=10;
+var y=5;
+ function add(){
+    alert(x+y);
+ }
+
+ function sub(){
+    // console.log(x-y);
+    alert(x-y);
+ }
+ function multi(){
+    alert(x*y);
+
+ }
+ function divi(){
+    alert(x/y);
+ }
---
+++ 18-02-23/script.js
@@ -0,0 +1,21 @@
+var bbb="vp22";
+
+var max= document.getElementById("js");
+// document.getElementById("data").innerHTML=bbb;
+
+
+
+function greetings(){
+   max.innerHTML="Hello"+bbb;
+    
+}
+function changecolor(){
+    max.style.color="red";
+}
+function hide(){
+    max.style.display="none";
+}
+
+function show(){
+    max.style.display="block";
+}
