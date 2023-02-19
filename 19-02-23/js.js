4ce1163ed7fe381110995c111dc7c6fe01c0cc18
Abinaya-raj26 <abinayaraj216@gmail.com>
2023-02-19 09:33:09 -0800

    19-02-23(2)

---
+++ 19-02-23/js.js
@@ -0,0 +1,16 @@
+var n=5;
+var m=3;
+// var z=document.getByElementId("marks")
+
+var z=document.getElementById("marks")
+
+function add(){
+    // alert(n+m);
+    console.log(n+m);
+    z.innerHTML=(n+m);
+}
+function sub(){
+    alert(n-m);
+    z.innerHTML=(n-m);
+}
+
