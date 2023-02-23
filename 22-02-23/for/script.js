a3c039c1a2e598e747407a435cff940cac6d799c
Abinaya-raj26 <abinayaraj216@gmail.com>
2023-02-22 22:48:41 -0800

    23-02-23

---
+++ 22-02-23/for/script.js
@@ -0,0 +1,21 @@
+var x=5;
+
+document.getElementById("x").innerHTML=x;
+
+for(var i=1;i<=x;i++){
+    // console.log("hello");
+    alert("hello "+i)
+    // document.getElementById("y").innerHTML=i;
+  
+}
+
+
+
+
+//  1. i varibale init value 0
+//  2. i value 
+// i++ => i+1
+
+
+// cycle 1 => i=1 => i++ => alert hello(i vlaue 1) =>end of the loop i=2 
+// cycle 
--- 22-02-23/script.js
+++ 22-02-23/script.js
@@ -1,43 +1,44 @@
-var username='vanipriya';
-var mark=77;
-var  x=document.getElementById("username");
-var  y=document.getElementById("score");
-var  z=document.getElementById("status");
+var username = 'vanipriya';
+var mark = 77;
+var x = document.getElementById("username");
+var y = document.getElementById("score");
+var z = document.getElementById("status");
 
-function showresults(){
-    x.innerHTML=username;
-    y.innerHTML=mark;   
+function showresults() {
+  x.innerHTML = username;
+  y.innerHTML = mark;
 
-    if(mark>=0 && mark<=100 && mark!=null){
-     if(mark>=0 && mark<50){
-          z.innerHTML="Fail";
-          z.style.color="red";
-              }
-       else if(mark>=50 && mark<60){
-        z.innerHTML="E-grade";
-        z.style.color="green";
-       }       
-     else if(mark>=60 && mark<70){
-        z.innerHTML="D-grade";
-        z.style.color="green";
-       }
-       else if(mark>=70 && mark<80){
-        z.innerHTML="C-grade";
-        z.style.color="green";
-       }
-       else if(mark>=80 && mark<90){
-        z.innerHTML="B-grade";
-        z.style.color="green";
-       }
-       else if(mark>=90 && mark<=100){
-        z.innerHTML="A-grade";
-        z.style.color="green";
-       }
+  if (mark >= 0 && mark <= 100 && mark != null) {
+    if (mark >= 0 && mark < 50) {
+      z.innerHTML = "Fail";
+      z.style.color = "red";
     }
-    else if(mark<0 || mark==null){
-      z.innerHTML="Invalid marks";
+    else if (mark >= 50 && mark < 60) {
+      z.innerHTML = "E-grade";
+      z.style.color = "green";
     }
-    else {
-   z.innerHTML="Invalid format";
+    else if (mark >= 60 && mark < 70) {
+      z.innerHTML = "D-grade";
+      z.style.color = "green";
     }
+    else if (mark >= 70 && mark < 80) {
+      z.innerHTML = "C-grade";
+      z.style.color = "green";
+    }
+    else if (mark >= 80 && mark < 90) {
+      z.innerHTML = "B-grade";
+      z.style.color = "green";
+    }
+    else if (mark >= 90 && mark <= 100) {
+      z.innerHTML = "A-grade";
+      z.style.color = "green";
+    }
+
+  }
+  else if (mark < 0 || mark == null) {
+    z.innerHTML = "Invalid marks";
+  }
+  else {
+    z.innerHTML = "Invalid format";
+  }
 }
