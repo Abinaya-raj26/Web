30e742f1c6fec6b2a74e46f98ddc7d0adc487584
Abinaya-raj26 <abinayaraj216@gmail.com>
2024-01-10 08:12:35 -0800

    10-1-24

---
+++ Java/firstjav.java
@@ -0,0 +1,17 @@
+
+public class firstjav {
+public static void main (String [] args) {
+    byte number1 = 127 ;
+    short number2 = 190 ; 
+    int number3 = 17090894 ;
+    long number4 =12345678901243L ;
+    // point numbers
+    float number5 = 12.6f;
+    double number6= 12334.3888888;
+//    alphabet
+  char alphabet = 'a';
+  boolean bool = false; 
+        System.out.println(number4);
+        System.out.println(bool);
+    }
+}
