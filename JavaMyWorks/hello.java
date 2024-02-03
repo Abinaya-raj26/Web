6e345f8c7800f2485e260a310d4c8d4d66340bd2
Abinaya-raj26 <abinayaraj216@gmail.com>
2024-02-03 04:03:24 -0800

    3-2-24

---
+++ JavaMyWorks/hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    public static void main(String[] args0) {
+        for(int i = 1; i<=5 ; i++){
+    
+            System.out.println("Hello"+i);
+             
+            for(int j=1; j<=5; j++)
+            {
+              System.out.println("" + (j+8)+ "-"+ j+9);
+            }
+           }  
+    }
+}
