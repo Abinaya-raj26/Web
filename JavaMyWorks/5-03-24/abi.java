473a58e39b8ddd5761b9f6863411bc28d7f8fb00
Abinaya-raj26 <abinayaraj216@gmail.com>
2024-02-05 05:19:09 -0800

    5-03-24

---
+++ JavaMyWorks/5-03-24/abi.java
@@ -0,0 +1,33 @@
+class computer {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public double add(double new1, double new2) {
+        return new1 + new2;
+    }
+}
+
+public class abi {
+ public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 5;
+    int num3 =5;
+    double new1 = 9.9;
+    double new2 = 1.1;
+
+    computer com=new computer();
+    int r= com.add(num1, num2);
+    int g= com.add(num1, num2, num3);
+    double p= com.add (new1, new2);
+
+    System.out.println(r);
+    System.out.println(g);
+    System.out.println(p);
+    
+ }
+}
