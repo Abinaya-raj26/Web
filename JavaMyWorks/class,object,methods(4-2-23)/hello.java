e2f5c70be86ae804c13cd5d4b9d0a8f79e0676ac
Abinaya-raj26 <abinayaraj216@gmail.com>
2024-02-05 04:55:51 -0800

    5-3-24

--- JavaMyWorks/class,object,methods(4-2-23)/hello.java
+++ JavaMyWorks/class,object,methods(4-2-23)/hello.java
@@ -1,27 +1,24 @@
-class orange
-{
-  public int juice(int org1, int org2){
-  int r=org1+org2;
-  if(org1<=5){
-    System.out.println("Orange juice Bhaiya");
+
+class computer {
+
+  public void playgmes() {
+    System.out.println("Playing games");
   }
-  else{
-    System.out.println( r);
-  }
-   return r;
+
+  public String GetMejoystick(int cost) {
+    if (cost >= 100)
+      return "joystick";
+    else
+      return "Better luck next time";
   }
 }
 
-
-
-
 public class hello {
-    public static void main(String[] args0) {
-       int orange1=1;
-       int orange2=20;
+  public static void main(String[] args0) {
 
-  orange org= new orange();
-   int result= org.juice(orange1, orange2) ;
-   System.out.println(result);
-    }
+    computer comp = new computer();
+    comp.playgmes();
+    String str = comp.GetMejoystick(10);
+   System.out.println(str);
   }
+}
